258a21017dd1140090009e8ebf2fc8d3e0a839c6
zhangyinkai <zhangyinkai@nmrbt.com>
2023-05-09 17:25:10 +0800

    多个页面结构

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
-
+import {resolve} from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react()],
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                exp: resolve(__dirname, 'exp/index.html'),
+            },
+        },
+    },
+    server: {
+        proxy: {
+            "/exp": {
+                target: "http://localhost:5173/exp", // 凡是遇到 /api 路径的请求，都映射到 target 属性
+                changeOrigin: true,
+                rewrite: (path) => path+='/index.html', // 重写 api 为 空，就是去掉它
+            },
+        },
+    }
 })
